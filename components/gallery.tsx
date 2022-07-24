67507bce81b86de44e31d568b6153f5e89be4d9b
charlie <charliezm88@gmail.com>
2022-07-24 11:24:16 +0800

    implement basic viewer page

---
+++ components/gallery.tsx
@@ -0,0 +1,357 @@
+import { RefObject, useEffect, useRef, useState } from "react";
+import { usePointerEvents } from "../lib/hook/usePointerEvents";
+import styles from "./gallery.module.css";
+
+interface propType {
+  imgs:
+    | {
+        src: string;
+        height: number;
+        width: number;
+        title: string;
+      }[]
+    | undefined;
+  keyFrameEffects?: any;
+  imperativeHandle?: RefObject<any>;
+}
+
+export type imperativeHandleType = {
+  goNext: () => void;
+  goLast: () => void;
+  goToIndex: (newIndex: number) => void;
+};
+
+const kfe = {
+  enterLeft: [
+    {
+      offset: 0,
+      opacity: 0,
+      transform: `translate(0vw,0px)`,
+    },
+    {
+      offset: 1,
+      opacity: 1,
+      transform: `translate(0px,0px) `,
+    },
+  ],
+  enterRight: [
+    {
+      offset: 0,
+      opacity: 0,
+      transform: `translate(0px,0px)`,
+    },
+    {
+      offset: 1,
+      opacity: 1,
+      transform: `translate(0px,0px)`,
+    },
+  ],
+
+  leaveLeft: [
+    {
+      offset: 0,
+      opacity: 1,
+      transform: `translate(0px,0px) scale(1)`,
+    },
+    {
+      offset: 1,
+      opacity: 0,
+      transform: `translate(0,0px) scale(0.2,0.2)`,
+    },
+  ],
+  leaveRight: [
+    {
+      offset: 0,
+      opacity: 1,
+      transform: `translate(0px,0px) scale(1)`,
+    },
+    {
+      offset: 1,
+      opacity: 0,
+      transform: `translate(0,0px) scale(0.2,0.2)`,
+    },
+  ],
+};
+
+const duration = 1000;
+export default function Gallery({ imgs }: propType) {
+  const [active, setActive] = useState<number>(0);
+  const [target, setTarget] = useState<number>(0);
+
+  const listRef = useRef<HTMLUListElement>(null);
+  const overlayUIRef = useRef<HTMLDivElement>(null);
+
+  const liElements = useLiElements(listRef);
+
+  useEffect(() => {
+    if (imgs && liElements) {
+      if (active !== target) {
+        //play enter and leave animation on new active Index
+        playAnimation(liElements, target, active, (t) => {
+          setActive(target);
+        });
+      }
+      //hide irrelevent imgs
+      liElements
+        .filter((e, index) => {
+          return index !== active && index !== target;
+        })
+        .forEach((ele) => {
+          ele.animate([{ transform: `translate(-110vw,0px)`, offset: 1 }], {
+            fill: "both",
+          });
+        });
+    }
+  }, [liElements, active, target]);
+
+  const [inDetail, setInDetail] = useState(false);
+
+  const [Pos, setPos] = useState([0, 0]);
+  const [initialPos, setInitialPos] = useState([0, 0]);
+  usePointerEvents(liElements?.at(active), {
+    onDown(e) {
+      setInitialPos([e.clientX, e.clientY]);
+    },
+    onMove(e) {
+      const liEle = liElements?.at(active);
+      if (liEle && inDetail) {
+        setPos([Pos[0] + e.movementX, Pos[1] + e.movementY]);
+        const translate = `translate(${Pos[0]}px,${Pos[1]}px)`;
+        // liEle.style.transform = `${translate}`;
+        liEle.animate([{ transform: translate, offset: 1 }], {
+          duration: 20,
+          fill: "forwards",
+        });
+      }
+    },
+    onUp(e) {
+      if (e.clientX === initialPos[0] && e.clientY === initialPos[1]) {
+        playEnlargeAnimation();
+      }
+    },
+    onCancel(e) {},
+  });
+
+  useEffect(() => {
+    const handler = (ev: { code: any }) => {
+      console.log(ev.code);
+      console.log(`active:${active},inDetail:${inDetail}`);
+      switch (ev.code) {
+        case `ArrowRight`: {
+          if (
+            imgs &&
+            active < imgs.length - 1 &&
+            active === target &&
+            !inDetail
+          ) {
+            setTarget(active + 1);
+          }
+          break;
+        }
+
+        case `ArrowLeft`: {
+          if (imgs && active > 0 && active === target && !inDetail) {
+            console.log("left");
+            setTarget(active - 1);
+          }
+          break;
+        }
+        case `Escape`: {
+          if (inDetail) {
+            playEnlargeAnimation();
+          }
+        }
+        default:
+          break;
+      }
+    };
+    window.addEventListener("keydown", handler);
+    return () => {
+      window.removeEventListener("keydown", handler);
+    };
+  }, [active, target, inDetail]);
+
+  return (
+    <>
+      <div
+        style={{ opacity: inDetail ? 0 : 1 }}
+        ref={overlayUIRef}
+        className={styles.overlayUI}
+      >
+        <header className={styles.header}>
+          <p>
+            {active + 1} / {imgs?.length}
+          </p>
+          <div></div>
+          <div className={styles.headerButtons}>
+            <button>
+              <a href={imgs?.at(active)?.src} download>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className={styles.download}
+                >
+                  <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                  <polyline points="7 10 12 15 17 10"></polyline>
+                  <line x1="12" y1="15" x2="12" y2="3"></line>
+                </svg>
+              </a>
+            </button>
+            <button
+              onClick={(e) => {
+                playEnlargeAnimation();
+              }}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <circle cx="11" cy="11" r="8"></circle>
+                <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                <line x1="11" y1="8" x2="11" y2="14"></line>
+                <line x1="8" y1="11" x2="14" y2="11"></line>
+              </svg>
+            </button>
+            <button
+              onClick={(e) => {
+                window.history.back();
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                className={styles.close}
+              >
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
+              </svg>
+            </button>
+          </div>
+        </header>
+        {imgs && imgs.length > 1 ? (
+          <>
+            <button
+              className={styles.slideButton + " " + styles.next}
+              onClick={(e) => {
+                if (imgs && active < imgs.length - 1 && !inDetail)
+                  setTarget(active + 1);
+              }}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <polyline points="9 18 15 12 9 6"></polyline>
+              </svg>
+            </button>
+            <button
+              className={styles.slideButton + " " + styles.prior}
+              onClick={(e) => {
+                if (imgs && active > 0 && !inDetail) setTarget(active - 1);
+              }}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <polyline points="15 18 9 12 15 6"></polyline>
+              </svg>
+            </button>
+          </>
+        ) : (
+          <></>
+        )}
+
+        <footer className={styles.footer}></footer>
+      </div>
+
+      <main className={styles.main}>
+        <ul className={styles.mainList} ref={listRef}>
+          {imgs?.map((img, index) => {
+            return (
+              <li
+                key={index}
+                style={{ cursor: inDetail ? "zoom-out" : "zoom-in" }}
+              >
+                <img src={img.src} alt={img.title} className={styles.img}></img>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+    </>
+  );
+  function playEnlargeAnimation() {
+    setPos([0, 0]);
+    if (liElements && imgs && target === active) {
+      const imgElement = liElements[active].querySelector("img");
+      if (!inDetail) {
+        console.log(`enlarging`);
+        if (imgElement) {
+          const rect = imgElement.getBoundingClientRect();
+          console.log(rect);
+          console.log(`enlargeScale:${imgs[active].width / rect.width}`);
+          imgElement.style.transform = `scale(${
+            imgs[active].width / rect.width
+          })`;
+          setInDetail(true);
+        }
+      } else {
+        console.log("shrinking");
+        liElements[active].animate(
+          { transform: "translate(0,0)" },
+          { duration: 200, fill: "forwards" }
+        );
+        if (imgElement) {
+          imgElement.style.transform = `scale(1)`;
+        }
+        setInDetail(false);
+      }
+    }
+  }
+
+  function useLiElements(listRef: RefObject<HTMLUListElement>) {
+    const [items, setItems] = useState<Array<HTMLLIElement>>();
+    useEffect(() => {
+      const list = listRef.current;
+      if (list) {
+        const collection = list.getElementsByTagName("li");
+        const itemArray = [];
+        for (let i = 0; i != collection.length; ++i) {
+          const item = collection.item(i);
+          item && itemArray.push(item);
+        }
+        setItems(itemArray);
+      }
+    }, [imgs]);
+    return items;
+  }
+}
+function playAnimation(
+  liElements: HTMLLIElement[],
+  target: number,
+  active: number,
+  onAnimationComplete: (reachedTarget: number) => void
+) {
+  const enterElement = liElements[target];
+  enterElement.style.zIndex = `1`;
+  const enterAni = enterElement.animate(
+    target > active ? kfe.enterRight : kfe.enterLeft,
+    {
+      duration,
+      fill: "both",
+      easing: "ease-out",
+    }
+  );
+
+  const leaveElement = liElements[active];
+  leaveElement.style.transform = `translate(0,0)`;
+  const leaveAni = leaveElement.animate(
+    target > active ? kfe.leaveLeft : kfe.leaveRight,
+    {
+      duration,
+      fill: "both",
+      easing: "ease-out",
+    }
+  );
+
+  enterAni.play();
+  leaveAni.play();
+  // enterAni.commitStyles();
+  // leaveAni.commitStyles();
+  Promise.all([enterAni.finished, leaveAni.finished]).then((e) => {
+    console.log("playback Finished");
+    onAnimationComplete(target);
+  });
+}
--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -2,12 +2,8 @@ import * as api from "../../lib/strapiLib";
 import { checkAndSetEV } from "../../lib/strapiUtil";
 
 import { GetStaticPaths, GetStaticProps } from "next";
+import Gallery from "../../components/gallery";
 import Head from "next/head";
-import { useEffect, useRef, useState } from "react";
-
-import styles from "../../styles/Post.module.css";
-import SwipeComponent from "../../components/swipeComponent";
-import useGallery, { imperativeHandleType } from "../../lib/hook/useGallery";
 
 export default function Post({
   post,
@@ -16,236 +12,31 @@ export default function Post({
   post: api.PostResponse;
   assetEndpoint: string;
 }) {
-  const loop = false;
-  const listRef = useRef<HTMLUListElement>(null);
-
-  const [selected, setSelected] = useState(0);
-  const [target, setTarget] = useState(0);
-
-  const numOfImage = post.data?.attributes?.Images?.data?.length ?? 0;
-
-  function useLiElements() {
-    const [items, setItems] = useState<Array<HTMLLIElement>>();
-    useEffect(() => {
-      const list = listRef.current;
-      if (list) {
-        const collection = list.getElementsByTagName("li");
-        const itemArray = [];
-        for (let i = 0; i != collection.length; ++i) {
-          const item = collection.item(i);
-          item && itemArray.push(item);
-        }
-        setItems(itemArray);
-      }
-    }, [post]);
-    return items;
-  }
-  const ItemElements = useLiElements();
-
-  let selectedElement = null;
-  let priorElement = null;
-  let nextElement = null;
-  if (ItemElements) {
-    selectedElement = ItemElements[selected];
-    if (selected < ItemElements.length - 1) {
-      nextElement = ItemElements[selected + 1];
-    }
-    if (selected > 0) {
-      priorElement = ItemElements[selected - 1];
-    }
-  }
-
-  const galleryImperativeHandle = useRef<imperativeHandleType>(null);
-
-  const kfe = {
-    enter: [
-      {
-        offset: 0,
-        opacity: 1,
-        transform: `translate(50vw,0px)`,
-      },
-      {
-        offset: 1,
-        opacity: 1,
-        transform: `translate(0px,0px) scale(1)`,
-      },
-    ],
-
-    leave: [
-      {
-        offset: 0,
-        opacity: 1,
-        transform: `translate(0px,0px) scale(1)`,
-      },
-      {
-        offset: 1,
-        opacity: 0,
-        transform: `translate(30vw,0px) scale(0.2,0.2)`,
-      },
-    ],
-  };
-
-  const [inDetailView, setInDetailView] = useState(false);
-  useGallery({
-    allElements: ItemElements,
-    activeIndex: target,
-    detailView: inDetailView,
-    callbacks: {
-      onAnimationFinished(oldIndex, newIndex) {
-        setSelected(newIndex);
-      },
-    },
-    imperativeHandle: galleryImperativeHandle,
-    keyFrameEffects: kfe,
+  const imageData = post.data?.attributes?.Images?.data?.map((img, index) => {
+    const url = img.attributes?.url!;
+    return {
+      src: `${assetEndpoint}${url}`,
+      width: img.attributes?.width!,
+      height: img.attributes?.height!,
+      title: img.attributes?.name!,
+    };
   });
 
+  const options = {
+    dataSource: imageData,
+  };
   const images = post.data?.attributes?.Images?.data;
   if (images)
     return (
       <>
         <Head>
-          {" "}
-          <title>{`${post.data?.attributes?.title} - 悠画廊`}</title>
+          <title>{post.data?.attributes?.title}</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
         </Head>
-        <header className={styles.header}>
-          <p>
-            {selected + 1} / {images.length}
-          </p>
-
-          <div className={styles.buttons}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              className={styles.close}
-            >
-              <line x1="18" y1="6" x2="6" y2="18"></line>
-              <line x1="6" y1="6" x2="18" y2="18"></line>
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              className={styles.download}
-            >
-              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-              <polyline points="7 10 12 15 17 10"></polyline>
-              <line x1="12" y1="15" x2="12" y2="3"></line>
-            </svg>
-          </div>
-        </header>
-        <main className={styles.main}>
-          <ul className={styles.mainList} ref={listRef}>
-            {" "}
-            {post.data?.attributes?.Images?.data?.map((img, index) => {
-              const url = img.attributes?.url!;
-              // const width = img.attributes?.width!;
-              // const height = img.attributes?.height!;
-
-              return (
-                <li key={img.id} id={img.id}>
-                  <img
-                    src={`${assetEndpoint}${url}`}
-                    alt={img.attributes?.alternativeText}
-                    className={styles.img}
-                  ></img>
-                </li>
-              );
-            })}
-          </ul>
-          <button
-            className={styles.nextbutton}
-            onClick={(e) => {
-              if (
-                ItemElements &&
-                selected < ItemElements.length - 1 &&
-                target === selected
-              ) {
-                setTarget(selected + 1);
-              }
-            }}
-          >
-            下一张
-          </button>
-
-          <button
-            className={styles.lastbutton}
-            onClick={(e) => {
-              if (ItemElements && selected > 0 && target === selected) {
-                setTarget(selected - 1);
-              }
-            }}
-          >
-            上一张
-          </button>
-          <button
-            className={styles.detailButton}
-            onClick={(e) => {
-              if (ItemElements && target === selected) {
-                setInDetailView(!selected);
-              }
-            }}
-          >
-            浏览大图
-          </button>
-          {/* {selectedElement ? (
-            <SwipeComponent
-              selectedElement={selectedElement}
-              nextElement={nextElement}
-              priorElement={priorElement}
-              callbacks={{
-                onSwipeLeft() {
-                  if (selected < numOfImage - 1)
-                    setSelected((v) => {
-                      return v + 1;
-                    });
-                },
-                onSwipeRight() {
-                  if (selected > 0)
-                    setSelected((v) => {
-                      return v - 1;
-                    });
-                },
-              }}
-            ></SwipeComponent>
-          ) : (
-            <></>
-          )} */}
-        </main>
-
-        <footer className={styles.footer}>
-          <h1 className={styles.title}>{post.data?.attributes?.title}</h1>
-          <p className={styles.discription}>
-            {post.data?.attributes?.discription}{" "}
-            <span className={styles.time}>
-              发表于{post.data?.attributes?.createdAt}
-            </span>
-          </p>
-        </footer>
-
-        {/* <footer className={styles.footer}>
-            <ul className={styles.thumbnailList} ref={listRef}>
-              {" "}
-              {post.data?.attributes?.Images?.data?.map((img, index) => {
-                const url = img.attributes?.url!;
-                const width = img.attributes?.width!;
-                const height = img.attributes?.height!;
-
-                return (
-                  <li
-                    className={`${styles.thumbnail} ${
-                      img.id === selected ? styles.focus : ""
-                    }`}
-                    key={img.id}
-                  >
-                    <img
-                      src={`${assetEndpoint}${url}`}
-                      alt={img.attributes?.alternativeText}
-                      className={styles.img}
-                    ></img>
-                  </li>
-                );
-              })}
-            </ul>
-          </footer> */}
+        <Gallery imgs={imageData}></Gallery>
       </>
     );
 }
