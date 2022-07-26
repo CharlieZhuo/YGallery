021fc24630ae3811a00d352b86de7773050f22d0
charlie <charliezm88@gmail.com>
2022-07-26 16:53:50 +0800

    add google analytics script

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +8,17 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-SZKP3XYDBS"
+          strategy="afterInteractive"
+        ></Script>
+        <Script strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SZKP3XYDBS');`}
+        </Script>
       </body>
     </Html>
   );
